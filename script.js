82f226e7215e7f1f66858404f58cd23ab82ee6b4
Sleanux <105379058+RealSleanux@users.noreply.github.com>
2025-03-02 22:21:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    var menuButton = document.querySelector('.menu-button');
+    var menu = document.querySelector('.menu')
+    menuButton.classList.toggle('open');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+}
